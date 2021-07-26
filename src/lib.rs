456ec5bc6d4d7608c7b600cfca414be9df31655c
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-07-26 21:48:34 +0200

    tests(src/lib.rs): add unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,37 @@
+mod parser;
+
+pub use parser::Args;
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn check_get_str() {
+        let arg = Args::new("s*", vec![
+            String::from("-s"), String::from("abc")]);
+        assert!(arg.get_str("s").eq("abc"));
+    }
+    #[test]
+    fn check_get_i32() {
+        let arg = Args::new("i#", vec![
+            String::from("-i"), String::from("1")]);
+        assert_eq!(arg.get_i32("i"), 1);
+    }
+    #[test]
+    fn check_get_bool() {
+        let arg = Args::new("b", vec![
+            String::from("-b")]);
+        assert!(arg.get_bool("b"));
+    }
+
+    #[test]
+    fn check_get_for_all_types() {
+        let arg = Args::new("i#,s*,b", vec![
+            String::from("-i"), String::from("1"), 
+            String::from("-b"), 
+            String::from("-s"), String::from("abc")]);
+            assert_eq!(arg.get_i32("i"), 1);
+            assert!(arg.get_bool("b"));
+            assert!(arg.get_str("s").eq("abc"));
     }
 }
