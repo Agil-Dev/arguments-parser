cfb39dd80c6248fd919d54f15e772fb9b2e035ee
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-07-28 22:18:05 +0200

    tests(src/lib.rs): add tests * check_is_argument_bool * check_is_argument_i32 * check_is_argument_string

--- src/lib.rs
+++ src/lib.rs
@@ -5,27 +5,49 @@ pub use parser::Args;
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
-    fn check_get_str() {
+    fn check_is_argument_bool() { 
+        let arg = Args::new("b", vec![
+            String::from("-b")]);
+        assert!(arg.is_bool("b"));
+    }
+    #[test]
+    fn check_is_argument_i32() { 
+        let arg = Args::new("i#", vec![
+            String::from("-i"), String::from("1")]);
+        assert!(arg.is_i32("i"));
+    }
+    #[test]
+    fn check_is_argument_string() { 
+        let arg = Args::new("s*", vec![
+            String::from("-s"), String::from("abc")]);
+        assert!(arg.is_str("s"));
+    }
+
+    #[test]
+    fn try_get_str() {
         let arg = Args::new("s*", vec![
             String::from("-s"), String::from("abc")]);
         assert!(arg.get_str("s").eq("abc"));
     }
+
     #[test]
-    fn check_get_i32() {
+    fn try_get_i32() {
         let arg = Args::new("i#", vec![
             String::from("-i"), String::from("1")]);
         assert_eq!(arg.get_i32("i"), 1);
     }
+
     #[test]
-    fn check_get_bool() {
+    fn try_get_bool() { 
         let arg = Args::new("b", vec![
             String::from("-b")]);
         assert!(arg.get_bool("b"));
     }
 
     #[test]
-    fn check_get_for_all_types() {
+    fn try_get_for_all_types() {
         let arg = Args::new("i#,s*,b", vec![
             String::from("-i"), String::from("1"), 
             String::from("-b"), 
