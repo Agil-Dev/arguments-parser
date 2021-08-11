560dc8153cc146c7c695a0e9b2525eed9abdd69e
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-08-11 17:44:32 +0200

    refactor(parser/mod.rs): remove duplicated code I forgot to delete it earlier

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -3,29 +3,6 @@ mod utils;
 use utils::*;
 use std::collections::HashMap;
 
-pub fn is_some<T>(opt: Option<T>) -> bool{
-    match opt {
-        Some(_) => true,
-        None => false,
-    }
-}
-pub fn cut_last_char(s: &str) -> &str {
-    let mut chars = s.chars();
-    chars.next_back();
-    chars.as_str()
-}
-pub fn cut_first_char(s: &str) -> &str {
-    let mut chars = s.chars();
-    chars.next();
-    chars.as_str()
-}
-pub fn get_arg_value(args: Vec<String>, key: &str) -> String {
-    match args.iter().position(|n| format!("-{}", key).eq(n)) {
-        Some(v) => args[v+1].to_string(),
-        None => "".to_string(),
-    }
-}
-
 pub struct Args {
     strings: HashMap<String, String>,
     bools:   HashMap<String, bool>,
